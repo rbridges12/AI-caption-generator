b7b01e25f8dedad4483fa8036b270331eafd62f6
Riley Bridges <rlybrdgs@gmail.com>
2021-04-17 08:49:26 -0500

    Initial commit

---
+++ caption_ai_test.py
@@ -0,0 +1,159 @@
+import requests
+
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# train image caption AI based off of provided dataset
+# to make it easier we could train it off of a smaller dataset
+#   - food
+#   - nature
+#   - respond to a person
+
+# website where you can upload an image or provide a link to an image
+# feed the image to the AI to get caption
+# if we have extra time, we can train multiple models to give captions with different moods
+# use twitter API to tweet image with caption
+# get link to the post
+
+from os import listdir
+from pickle import dump
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.applications.vgg16 import preprocess_input
+from keras.models import Model
+
+# extract features from each photo in the directory
+
+
+def extract_features(directory):
+    # load the model
+    model = VGG16()
+    # re-structure the model
+    model = Model(inputs=model.inputs, outputs=model.layers[-2].output)
+    # summarize
+    print(model.summary())
+    # extract features from each photo
+    features = dict()
+    for name in listdir(directory):
+        # load an image from file
+        filename = directory + '/' + name
+        image = load_img(filename, target_size=(224, 224))
+        # convert the image pixels to a numpy array
+        image = img_to_array(image)
+        # reshape data for the model
+        image = image.reshape(
+            (1, image.shape[0], image.shape[1], image.shape[2]))
+        # prepare the image for the VGG model
+        image = preprocess_input(image)
+        # get features
+        feature = model.predict(image, verbose=0)
+        # get image id
+        image_id = name.split('.')[0]
+        # store feature
+        features[image_id] = feature
+        print('>%s' % name)
+    return features
+
+
+# extract features from all images
+directory = 'Flickr8k_Dataset'
+features = extract_features(directory)
+print('Extracted Features: %d' % len(features))
+# save to file
+dump(features, open('features.pkl', 'wb'))
+# load doc into memory
+
+
+def load_doc(filename):
+	# open the file as read only
+	file = open(filename, 'r')
+	# read all text
+	text = file.read()
+	# close the file
+	file.close()
+	return text
+
+# extract descriptions for images
+
+
+def load_descriptions(doc):
+	mapping = dict()
+	# process lines
+	for line in doc.split('\n'):
+		# split line by white space
+		tokens = line.split()
+		if len(line) < 2:
+			continue
+		# take the first token as the image id, the rest as the description
+		image_id, image_desc = tokens[0], tokens[1:]
+		# remove filename from image id
+		image_id = image_id.split('.')[0]
+		# convert description tokens back to string
+		image_desc = ' '.join(image_desc)
+		# create the list if needed
+		if image_id not in mapping:
+			mapping[image_id] = list()
+		# store description
+		mapping[image_id].append(image_desc)
+	return mapping
+
+
+def clean_descriptions(descriptions):
+	# prepare translation table for removing punctuation
+	table = str.maketrans('', '', string.punctuation)
+	for key, desc_list in descriptions.items():
+		for i in range(len(desc_list)):
+			desc = desc_list[i]
+			# tokenize
+			desc = desc.split()
+			# convert to lower case
+			desc = [word.lower() for word in desc]
+			# remove punctuation from each token
+			desc = [w.translate(table) for w in desc]
+			# remove hanging 's' and 'a'
+			desc = [word for word in desc if len(word) > 1]
+			# remove tokens with numbers in them
+			desc = [word for word in desc if word.isalpha()]
+			# store as string
+			desc_list[i] = ' '.join(desc)
+
+# convert the loaded descriptions into a vocabulary of words
+
+
+def to_vocabulary(descriptions):
+	# build a list of all description strings
+	all_desc = set()
+	for key in descriptions.keys():
+		[all_desc.update(d.split()) for d in descriptions[key]]
+	return all_desc
+
+# save descriptions to file, one per line
+
+
+def save_descriptions(descriptions, filename):
+	lines = list()
+	for key, desc_list in descriptions.items():
+		for desc in desc_list:
+			lines.append(key + ' ' + desc)
+	data = '\n'.join(lines)
+	file = open(filename, 'w')
+	file.write(data)
+	file.close()
+
+
+filename = 'Flickr8k_text/Flickr8k.token.txt'
+# load descriptions
+doc = load_doc(filename)
+# parse descriptions
+descriptions = load_descriptions(doc)
+print('Loaded: %d ' % len(descriptions))
+# clean descriptions
+clean_descriptions(descriptions)
+# summarize vocabulary
+vocabulary = to_vocabulary(descriptions)
+print('Vocabulary Size: %d' % len(vocabulary))
+# save to file
+save_descriptions(descriptions, 'descriptions.txt')
